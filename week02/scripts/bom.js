ac67ca463333ae85ba52e168f9adfda4dd4216ed
LianRaflores-lab <lraflores@byupathway.edu>
2026-07-09 22:26:11 +0800

    week2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,23 @@
-const input = document.querySelector('#favchap')
-const button = document.querySelector('button')
-const list = document.querySelector('_______')
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const input = document.querySelector('#favchap')
+        const button = document.querySelector('button')
+        const list = document.querySelector('_______')
 
-const li = document.createElement('li')
-const deleteButton = document.createElement('button')
+        const li = document.createElement('li')
+        const deleteButton = document.createElement('button')
 
-li.textContent = input.value;
-deleteButton.textContent = '❌'
-li.append(deleteButton)
-li.append(list)
+        li.textContent = input.value;
+        deleteButton.textContent = '❌'
+        li.append(deleteButton)
+        li.append(list)
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+
+        input.value = ''
+        input.focus();
+    }
+});
