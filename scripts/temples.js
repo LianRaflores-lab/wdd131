d6c597792377b32928eb8eb148942861ec9fbb94
LianRaflores-lab <lraflores@byupathway.edu>
2026-07-10 18:10:46 +0800

    week2

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,10 @@ menuButton.addEventListener("click", () => {
     menuButton.classList.toggle("open");
     navigation.classList.toggle("open");
 });
+
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
