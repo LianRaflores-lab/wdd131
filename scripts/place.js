7b5051cb330b666ee4d0de73f31d835406f2f9a8
LianRaflores <lraflores@byupathway.edu>
2026-07-19 08:37:40 +0800

    changes

--- scripts/place.js
+++ scripts/place.js
@@ -9,17 +9,14 @@ const temperature = 31;
 const windSpeed = 10;
 
 function calculateWindChill(temp, speed) {
-    return 13.12 +
-       0.6215 * temp -
-       11.37 * Math.pow(speed, 0.16) +
-       0.3965 * temp * Math.pow(speed, 0.16);
+    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
 }
 
 if (temperature <= 10 && windSpeed > 4.8) {
     const windChill = calculateWindChill(temperature, windSpeed)
     document.getElementById("windChill").textContent = windChill.tofixeed(1) + "°C";
 }
+
 else {
     document.getElementById("windchill").textContent = "N/A";
 }
-
